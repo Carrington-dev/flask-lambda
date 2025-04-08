0d7207e1c69200c77975c5f8c8db2cb5a488b1ee
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-04-08 15:25:27 +0200

    Update app.py
    Added example lamdba function

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
+import logging
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-  return jsonify({"message": "Welcome back"}), 200
+app = Flask(__name__)
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+@app.route("/", methods=["GET", "POST"])
+def lambda_handler(event=None, context=None):
+    logger.info("Lambda function invoked index()")
+
+    return jsonify({"message": "Welcome back"}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
